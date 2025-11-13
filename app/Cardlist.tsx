ab7e19d729b18cf7ec529a64f040f2be7a6073d9
JesseChum <jessechum@icloud.com>
2025-11-13 14:24:13 -0800

    created card list. we need to make a card

--- app/Cardlist.tsx
+++ app/Cardlist.tsx
@@ -1,5 +1,9 @@
 export default function Cardlist({list}) {
     return(
-        
-    )
+        <div className="card">
+            {list.map((parseVideos) => (
+                <Card list = {list}/>
+            ))}
+        </div>
+    );
 }
